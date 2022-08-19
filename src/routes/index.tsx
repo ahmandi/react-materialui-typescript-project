cbb765cce458bc5ebe358a365be946059cbfd808
Amanda <amandard94@gmail.com>
2022-08-19 12:30:16 -0300

    Added react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/first-page" element={<p>First Page</p>} />
+			<Route path="*" element={<Navigate to="/first-page" />} />
+		</Routes>
+	);
+};
