c5926294c78aba3c1dd5fb718afde0728f261ed5
Amanda <amandard94@gmail.com>
2022-08-24 15:26:20 -0300

    Feat: Added Drawer

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,62 @@
+import {
+	Avatar,
+	Divider,
+	Drawer,
+	Icon,
+	List,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
+	useTheme,
+} from '@mui/material';
+import { Box } from '@mui/system';
+import { ReactNode } from 'react';
+
+interface IDrawerProps {
+	children: ReactNode;
+}
+
+export const MenuLateral = ({ children }: IDrawerProps) => {
+	const theme = useTheme();
+
+	return (
+		<>
+			<Drawer variant="permanent">
+				<Box
+					width={theme.spacing(28)}
+					height="100%"
+					display="flex"
+					flexDirection="column"
+				>
+					<Box
+						width="100%"
+						height={theme.spacing(20)}
+						display="flex"
+						alignItems="center"
+						justifyContent="center"
+					>
+						<Avatar
+							sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+							src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg"
+						/>
+					</Box>
+					<Divider />
+					<Box flex={1}>
+						<List component="nav">
+							<ListItemButton>
+								<ListItemIcon>
+									<Icon>home</Icon>
+								</ListItemIcon>
+								<ListItemText primary="Home page" />
+							</ListItemButton>
+						</List>
+					</Box>
+				</Box>
+			</Drawer>
+
+			<Box height="100vh" marginLeft={theme.spacing(28)}>
+				{children}
+			</Box>
+		</>
+	);
+};
